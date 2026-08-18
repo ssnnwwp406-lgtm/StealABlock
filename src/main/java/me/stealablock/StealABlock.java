88f44fe94a5d52ac73802642ef0181073410c4be
ssnnwwp406-lgtm <ssnnwwp406@gmail.com>
2026-08-19 02:47:49 +0300

    Refactor StealABlock class for clarity and structure

--- src/main/java/me/stealablock/StealABlock.java
+++ src/main/java/me/stealablock/StealABlock.java
@@ -1,5 +1,6 @@
 package me.stealablock;
 
+import me.stealablock.base.BaseManager;
 import me.stealablock.command.SABCommand;
 import me.stealablock.selection.SelectionManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,46 +8,62 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class StealABlock extends JavaPlugin {
 
     private SelectionManager selectionManager;
+    private BaseManager baseManager;
 
     @Override
     public void onEnable() {
 
         saveDefaultConfig();
 
-        selectionManager = new SelectionManager();
+        selectionManager =
+                new SelectionManager();
 
-        SABCommand sabCommand = new SABCommand(this);
+        baseManager =
+                new BaseManager(this);
+
+        SABCommand sabCommand =
+                new SABCommand(this);
 
         if (getCommand("sab") != null) {
-            getCommand("sab").setExecutor(sabCommand);
-            getCommand("sab").setTabCompleter(sabCommand);
+
+            getCommand("sab")
+                    .setExecutor(sabCommand);
+
+            getCommand("sab")
+                    .setTabCompleter(sabCommand);
         }
 
-        getServer().getPluginManager().registerEvents(
-                sabCommand,
-                this
-        );
+        getServer()
+                .getPluginManager()
+                .registerEvents(
+                        sabCommand,
+                        this
+                );
 
-        getLogger().info("=================================");
-        getLogger().info("        Steal A Block");
-        getLogger().info("        Plugin Enabled");
-        getLogger().info("=================================");
+        getLogger().info(
+                "Steal A Block enabled!"
+        );
     }
 
     @Override
     public void onDisable() {
 
-        if (getConfig().getBoolean(
-                "settings.save-on-disable",
-                true
-        )) {
-            saveConfig();
+        if (baseManager != null) {
+            baseManager.save();
         }
 
-        getLogger().info("Steal A Block disabled.");
+        saveConfig();
+
+        getLogger().info(
+                "Steal A Block disabled!"
+        );
     }
 
     public SelectionManager getSelectionManager() {
         return selectionManager;
+    }
+
+    public BaseManager getBaseManager() {
+        return baseManager;
     }
 }
