3e1f88290c99c85900f300917bc2b3bbafcdebe3
ssnnwwp406-lgtm <ssnnwwp406@gmail.com>
2026-08-19 03:12:26 +0300

    Add BlockManager and BlockSpawner to StealABlock

--- src/main/java/me/stealablock/StealABlock.java
+++ src/main/java/me/stealablock/StealABlock.java
@@ -1,6 +1,8 @@
 package me.stealablock;
 
 import me.stealablock.base.BaseManager;
+import me.stealablock.block.BlockManager;
+import me.stealablock.block.BlockSpawner;
 import me.stealablock.command.SABCommand;
 import me.stealablock.selection.SelectionManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,39 +12,40 @@ public class StealABlock extends JavaPlugin {
     private SelectionManager selectionManager;
     private BaseManager baseManager;
 
+    private BlockManager blockManager;
+    private BlockSpawner blockSpawner;
+
     @Override
     public void onEnable() {
 
         saveDefaultConfig();
 
-        selectionManager =
-                new SelectionManager();
+        // Selection
+        selectionManager = new SelectionManager();
 
-        baseManager =
-                new BaseManager(this);
+        // Bases
+        baseManager = new BaseManager(this);
 
-        SABCommand sabCommand =
-                new SABCommand(this);
+        // Blocks
+        blockManager = new BlockManager();
+
+        // Block Spawner
+        blockSpawner = new BlockSpawner(this);
+        blockSpawner.start();
+
+        // Commands
+        SABCommand sabCommand = new SABCommand(this);
 
         if (getCommand("sab") != null) {
-
-            getCommand("sab")
-                    .setExecutor(sabCommand);
-
-            getCommand("sab")
-                    .setTabCompleter(sabCommand);
+            getCommand("sab").setExecutor(sabCommand);
+            getCommand("sab").setTabCompleter(sabCommand);
         }
 
-        getServer()
-                .getPluginManager()
-                .registerEvents(
-                        sabCommand,
-                        this
-                );
+        // Events
+        getServer().getPluginManager()
+                .registerEvents(sabCommand, this);
 
-        getLogger().info(
-                "Steal A Block enabled!"
-        );
+        getLogger().info("StealABlock enabled!");
     }
 
     @Override
@@ -54,9 +57,7 @@ public class StealABlock extends JavaPlugin {
 
         saveConfig();
 
-        getLogger().info(
-                "Steal A Block disabled!"
-        );
+        getLogger().info("StealABlock disabled!");
     }
 
     public SelectionManager getSelectionManager() {
@@ -65,5 +66,13 @@ public class StealABlock extends JavaPlugin {
 
     public BaseManager getBaseManager() {
         return baseManager;
+    }
+
+    public BlockManager getBlockManager() {
+        return blockManager;
+    }
+
+    public BlockSpawner getBlockSpawner() {
+        return blockSpawner;
     }
 }
