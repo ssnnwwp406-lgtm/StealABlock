eb64ee9487f1fc15ef9836184c7d31ccb75625d2
ssnnwwp406-lgtm <ssnnwwp406@gmail.com>
2026-08-19 03:17:23 +0300

    Refactor StealABlock initialization and formatting

--- src/main/java/me/stealablock/StealABlock.java
+++ src/main/java/me/stealablock/StealABlock.java
@@ -1,9 +1,11 @@
 package me.stealablock;
 
+import me.stealablock.base.BaseLockManager;
 import me.stealablock.base.BaseManager;
 import me.stealablock.block.BlockManager;
 import me.stealablock.block.BlockSpawner;
 import me.stealablock.command.SABCommand;
+import me.stealablock.rebirth.RebirthManager;
 import me.stealablock.selection.SelectionManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,9 +13,12 @@ public class StealABlock extends JavaPlugin {
 
     private SelectionManager selectionManager;
     private BaseManager baseManager;
+    private BaseLockManager baseLockManager;
 
     private BlockManager blockManager;
     private BlockSpawner blockSpawner;
+
+    private RebirthManager rebirthManager;
 
     @Override
     public void onEnable() {
@@ -21,43 +26,77 @@ public class StealABlock extends JavaPlugin {
         saveDefaultConfig();
 
         // Selection
-        selectionManager = new SelectionManager();
+        selectionManager =
+                new SelectionManager();
 
         // Bases
-        baseManager = new BaseManager(this);
+        baseManager =
+                new BaseManager(this);
+
+        // Rebirth
+        rebirthManager =
+                new RebirthManager(this);
+
+        // Base Lock
+        baseLockManager =
+                new BaseLockManager(this);
 
         // Blocks
-        blockManager = new BlockManager();
+        blockManager =
+                new BlockManager();
 
         // Block Spawner
-        blockSpawner = new BlockSpawner(this);
+        blockSpawner =
+                new BlockSpawner(this);
+
         blockSpawner.start();
 
-        // Commands
-        SABCommand sabCommand = new SABCommand(this);
+        // Command
+        SABCommand sabCommand =
+                new SABCommand(this);
 
         if (getCommand("sab") != null) {
-            getCommand("sab").setExecutor(sabCommand);
-            getCommand("sab").setTabCompleter(sabCommand);
+
+            getCommand("sab")
+                    .setExecutor(sabCommand);
+
+            getCommand("sab")
+                    .setTabCompleter(sabCommand);
         }
 
         // Events
-        getServer().getPluginManager()
-                .registerEvents(sabCommand, this);
+        getServer()
+                .getPluginManager()
+                .registerEvents(
+                        sabCommand,
+                        this
+                );
 
-        getLogger().info("StealABlock enabled!");
+        getLogger().info(
+                "StealABlock enabled!"
+        );
     }
 
     @Override
     public void onDisable() {
 
+        if (baseLockManager != null) {
+            baseLockManager.shutdown();
+        }
+
         if (baseManager != null) {
             baseManager.save();
         }
 
+        if (rebirthManager != null) {
+            rebirthManager.save();
+        }
+
         saveConfig();
 
-        getLogger().info("StealABlock disabled!");
+        getLogger().info(
+                "StealABlock disabled!"
+        );
     }
 
     public SelectionManager getSelectionManager() {
@@ -68,11 +107,19 @@ public class StealABlock extends JavaPlugin {
         return baseManager;
     }
 
+    public BaseLockManager getBaseLockManager() {
+        return baseLockManager;
+    }
+
     public BlockManager getBlockManager() {
         return blockManager;
     }
 
     public BlockSpawner getBlockSpawner() {
         return blockSpawner;
+    }
+
+    public RebirthManager getRebirthManager() {
+        return rebirthManager;
     }
 }
